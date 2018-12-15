cbb8aa8941cc2b2fb6960d0fe1be0d799e22cba4
Christian <christian.r.s.244@gmail.com>
2018-12-15 22:26:46 +0100

    Chats view

--- client/src/Components/ChatService.js
+++ client/src/Components/ChatService.js
@@ -9,13 +9,18 @@ class ChatService {
         })
     }
 
+    getChats = () => {
+        return this.service.get("/getchats")
+            .then(response => response.data)
+    }
+
     getChat = (id) => {
         return this.service.get(`/getchat/${id}`)
             .then(response => response.data)
     }
-    addMessage = (id,message) =>{
-        return this.service.post(`/addmessage/${id}`,message)
-        .then(response => response.data)
+    addMessage = (id, message) => {
+        return this.service.post(`/addmessage/${id}`, message)
+            .then(response => response.data)
     }
 }
 
--- server/routes/chat.js
+++ server/routes/chat.js
@@ -1,22 +1,37 @@
 const express = require("express");
 const router = express.Router();
 const Chat = require("../models/Chat")
+const User = require("../models/User")
 
 
 router.get("/getchat/:_id", (req, res, next) => {
   Chat.findById(req.params._id).populate("users")
-  .then(chat => {
-    res.status(200).json({chat, user: req.user.username})
-  }).catch(err=>console.log(err))
+    .then(chat => {
+      res.status(200).json({ chat, user: req.user.username })
+    })
+    .catch(err => console.log(err))
 });
 
 router.post("/addmessage/:_id", (req, res, next) => {
-  Chat.findByIdAndUpdate(req.params._id, { $addToSet: { messages: {name: req.user.username, message: req.body.message} } }, { new: true })
-  .then(chat => {
-    res.status(200)
-  })
-  .catch(err=>console.log(err))
+  Chat.findByIdAndUpdate(req.params._id, { $addToSet: { messages: { name: req.user.username, message: req.body.message } } }, { new: true })
+    .then(chat => {
+      res.status(200)
+    })
+    .catch(err => console.log(err))
 });
-  
 
-  module.exports = router;
+router.get("/getchats", (req, res, next) => {
+  User.findById(req.user._id).populate("friendchats").populate("planchats")
+    .then(user => {
+      Chat.find({ _id: { $in: user.friendchats } }).populate("users")
+        .then(friendchats => {
+          Chat.find({ _id: { $in: user.planchats } }).populate("plan")
+            .then(planchats => res.status(200).json({ friendchats, planchats, user: req.user }))
+            .catch(err => console.log(err))
+        })
+        .catch(err => console.log(err))
+    })
+    .catch(err => console.log(err))
+})
+
+module.exports = router;

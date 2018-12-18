d6efcdcbafa5cf019f8c4c3919e57ac822e1cc5e
Christian <christian.r.s.244@gmail.com>
2018-12-18 20:09:51 +0100

    Tapbar

--- client/src/Components/Tapbar/Tapbar.jsx
+++ client/src/Components/Tapbar/Tapbar.jsx
@@ -1,16 +1,21 @@
 import React, { Component } from 'react'
 import { Link } from "react-router-dom";
 import "./Tapbar.css"
+import Home from "../../icons/TabBar/HomeColor.png"
+import MisPlanes from "../../icons/TabBar/MisPlanesColor.png"
+import Anadir from "../../icons/TabBar/AnadirPlanColor.png"
+import Conexiones from "../../icons/TabBar/ConexionsColor.png"
+import MiPerfil from "../../icons/TabBar/MiPerfilColor.png"
 
 export default class Tapbar extends Component {
   render() {
     return (
       <div className="Tapbar">
-        <div><Link to={"/plans"}>H</Link></div>
-        <div><Link to={"/plansgo"}>P</Link></div>
-        <div><Link to={"/newplan"}>+</Link></div>
-        <div><Link to={"/friends"}>F</Link></div>
-        <div><Link to={"/profile"}>M</Link></div>
+        <div><Link to={"/plans"}><img src={Home}/></Link></div>
+        <div><Link to={"/plansgo"}><img src={MisPlanes}/></Link></div>
+        <div><Link to={"/newplan"}><img src={Anadir}/></Link></div>
+        <div><Link to={"/friends"}><img src={Conexiones}/></Link></div>
+        <div><Link to={"/profile"}><img src={MiPerfil}/></Link></div>
       </div>
     )
   }

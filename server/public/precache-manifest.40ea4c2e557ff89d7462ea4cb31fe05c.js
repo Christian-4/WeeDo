389ccf7f131effe6672dbd2a84a959da61c07e25
Christian <christian.r.s.244@gmail.com>
2018-12-18 16:53:56 +0100

    Heroku deploy test

---
+++ server/public/precache-manifest.40ea4c2e557ff89d7462ea4cb31fe05c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "62b8e48ae2ff56927abd",
+    "url": "/static/css/main.20fc16e0.chunk.css"
+  },
+  {
+    "revision": "62b8e48ae2ff56927abd",
+    "url": "/static/js/main.62b8e48a.chunk.js"
+  },
+  {
+    "revision": "4d6195df7c86a46129b1",
+    "url": "/static/css/1.8f526593.chunk.css"
+  },
+  {
+    "revision": "4d6195df7c86a46129b1",
+    "url": "/static/js/1.4d6195df.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9a85790e17e85d484146c23cdfd0b168",
+    "url": "/index.html"
+  }
+];

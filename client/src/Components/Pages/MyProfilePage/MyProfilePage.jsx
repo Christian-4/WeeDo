75033ec0a9fe0e9440356476965849cd23fd9e86
Christian <christian.r.s.244@gmail.com>
2018-12-19 22:26:47 +0100

    MyProfile doing

--- client/src/Components/Pages/MyProfilePage/MyProfilePage.jsx
+++ client/src/Components/Pages/MyProfilePage/MyProfilePage.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import UserService from '../../UserService';
 import FormSinUp from '../../FormSingUp/FormSingUp.jsx';
 import { Link } from "react-router-dom";
+import "./MyProfilePage.css"
 import Logout from "../../Logout/Logout.jsx"
 import Nav from "../../Nav/Nav"
 
@@ -19,40 +20,50 @@ export default class ProfilePage extends Component {
   componentDidMount() {
 
     this.UserService.getUser()
-    .then(response => {
-        this.setState({...this.state, user: response.user})
-    })
+      .then(response => {
+        this.setState({ ...this.state, user: response.user })
+      })
   }
 
-  printUser = () =>{
+  printUser = () => {
     return (
       <React.Fragment>
-        <Nav/>
-        <section className="personal-data">
-          <div className="div-photo">
-           <img className="user-photo" src={this.state.user.image}></img>
+        <Nav />
+        <div className="myProfile">
+          <div className="myProfileHeader">
+            <img src={this.state.user.image} />
+            <div className="myProfileHeaderText">
+              <p className="name">{this.state.user.username}</p>
+              <p className="cumple">Cumpleaños:<br></br> 22 Junio, 21 años</p>
+            </div>
           </div>
-          {this.state.user.username}
-        </section>
-        <div>
-        <Logout></Logout>
-      </div>
+          <div className="sobreMi">
+            <label>Sobre Mi</label><br></br>
+            <textarea placeholder="Escribe una pequeña descripción sobre ti"></textarea>
+          </div>
+          <div className="educacionYTrabajo">
+            <label>Educación y trabajo</label>
+          </div>
+          <div>
+            <Logout></Logout>
+          </div>
+        </div>
+
 
       </React.Fragment>
-     
+
     )
   }
 
   render() {
-    return( 
+    return (
       <React.Fragment>
         {
-          this.state.user !== null && 
+          this.state.user !== null &&
           <div>{this.printUser()}</div>
-       
         }
-       
-        
+
+
       </React.Fragment>
     )
   }

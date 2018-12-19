a0cf6c8a42c0c85c31831e85b2c5cfd3c6fe9155
Christian <christian.r.s.244@gmail.com>
2018-12-19 19:17:37 +0100

    All users view, no finish searchbar & hobbies

--- client/src/Components/Pages/UserPage/UserPage.jsx
+++ client/src/Components/Pages/UserPage/UserPage.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import FriendsService from "../../FriendsService";
 import UserService from "../../UserService";
+import {Link} from "react-router-dom"
+import Nav from "../../Nav/Nav.jsx"
+import "./UserPage.css"
+import SearchIcon from "../../../icons/icons/search.png"
+
 
 export default class UserPage extends Component {
   constructor(props) {
@@ -39,19 +44,39 @@ export default class UserPage extends Component {
 
   printAllUsers = (addFriend, service, userSession) => {
     return (
-      <div>
-        {this.state.users.map(function(user) {
+      <div className="allUsers">
+        <img className="searchInput" src={SearchIcon} />
+        <input className="inputFindUsers" type="text" placeholder="Busca algún amigo"></input>
+        {this.state.users.map(function (user) {
           return (
-            <div>
-              <img src={user.image} width="50" height="50" />
-              <p>{user.username}</p>
-              {userSession[0].sendRequestUser.includes(user._id) ? (
-                <p>Request send</p>
-              ) : (
-                <button onClick={() => addFriend(user._id, service)}>
-                  Add Friend
+            <div className="allUsersCard">
+              <div className="allUsersCardTop">
+                <Link to={`/profile/${user._id}`}><img src={user.image} width="50" height="50" /></Link>
+                <Link to={`/profile/${user._id}`}><p>{user.username}</p></Link>
+              </div>
+              <div className="allUsersCardHobbies">
+                <p>Intereses del usuario</p>
+                <div className="allUsersCardHobbiesDivs">
+                  {user.hobbies.map(function (hobby) {
+                    console.log(hobby)
+                    return (
+                      <div className="allUsersCardHobby">
+                        <img src="" />
+                        <p>{hobby}</p>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+              <div>
+                {userSession[0].sendRequestUser.includes(user._id) ? (
+                  <button className="addFriendButton">Petición enviada</button>
+                ) : (
+                    <button className="addFriendButton" onClick={() => addFriend(user._id, service)}>
+                      Conectar
                 </button>
-              )}
+                  )}
+              </div>
             </div>
           );
         })}
@@ -62,6 +87,7 @@ export default class UserPage extends Component {
   render() {
     return (
       <React.Fragment>
+        <Nav></Nav>
         {this.state.users !== null && (
           <div>
             {this.printAllUsers(

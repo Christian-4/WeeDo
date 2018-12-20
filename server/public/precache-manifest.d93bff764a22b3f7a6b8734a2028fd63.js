cbb830288992a612d85a4341b243753c0f5fe2d8
Christian <christian.r.s.244@gmail.com>
2018-12-20 13:52:48 +0100

    Deploy heroku

---
+++ server/public/precache-manifest.d93bff764a22b3f7a6b8734a2028fd63.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "b35fd564e1b4c575a0a6",
+    "url": "/static/css/main.09a0fde3.chunk.css"
+  },
+  {
+    "revision": "b35fd564e1b4c575a0a6",
+    "url": "/static/js/main.b35fd564.chunk.js"
+  },
+  {
+    "revision": "f3d0da99eec38d48e96a",
+    "url": "/static/css/1.9734e49a.chunk.css"
+  },
+  {
+    "revision": "f3d0da99eec38d48e96a",
+    "url": "/static/js/1.f3d0da99.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "90c97245cb9b7a55409e686377938793",
+    "url": "/static/media/white.90c97245.png"
+  },
+  {
+    "revision": "dfacfe79ab1ba8b88350bb3b35e64fa3",
+    "url": "/static/media/notifications.dfacfe79.png"
+  },
+  {
+    "revision": "5d1d1308305b15e87ab947f12b6305da",
+    "url": "/static/media/saved.5d1d1308.png"
+  },
+  {
+    "revision": "4906010f03e5046c45b06406024ffe41",
+    "url": "/static/media/save.4906010f.png"
+  },
+  {
+    "revision": "bfb7e35a4915ca842aeb7cfee6785549",
+    "url": "/static/media/ConexionAdd.bfb7e35a.png"
+  },
+  {
+    "revision": "0115a137cb9d47f3ac9b0c25366fd51b",
+    "url": "/static/media/ConexionDecline.0115a137.png"
+  },
+  {
+    "revision": "bc740dcdd05ae6b770f3b21259b9f075",
+    "url": "/static/media/filterBars.bc740dcd.png"
+  },
+  {
+    "revision": "b12673a04044d9d6241036600bdf9973",
+    "url": "/static/media/chat.b12673a0.png"
+  },
+  {
+    "revision": "965a7a1a7bd29d1565b5edeca7b4b9b7",
+    "url": "/static/media/icons_mobiscroll.965a7a1a.woff"
+  },
+  {
+    "revision": "c405c13ebacaadfc5def2aa6f9ae7b7d",
+    "url": "/static/media/icons_mobiscroll.c405c13e.ttf"
+  },
+  {
+    "revision": "88ca0b761d4d9e27ba9c986cf7624aef",
+    "url": "/static/media/Group5.88ca0b76.png"
+  },
+  {
+    "revision": "4ed2563b69e3536be3eafa44b48ba05d",
+    "url": "/static/media/Group4.4ed2563b.png"
+  },
+  {
+    "revision": "ddf6f0486780d802b480aa45549fe3f6",
+    "url": "/static/media/HomeColor.ddf6f048.png"
+  },
+  {
+    "revision": "4abdd9e151a4fdcecddd16eda7ec6c2a",
+    "url": "/static/media/MisPlanesColor.4abdd9e1.png"
+  },
+  {
+    "revision": "6a5293dce3632b8a1bb13954adb952a1",
+    "url": "/static/media/AnadirPlanColor.6a5293dc.png"
+  },
+  {
+    "revision": "661e321a0d1ceefcd29f59f950b698f8",
+    "url": "/static/media/ConexionsColor.661e321a.png"
+  },
+  {
+    "revision": "0da4ca611a6834d46e7c359524f6ce9e",
+    "url": "/static/media/MiPerfilColor.0da4ca61.png"
+  },
+  {
+    "revision": "565500d1b5b1f30858d4149bea9026d2",
+    "url": "/index.html"
+  }
+];
